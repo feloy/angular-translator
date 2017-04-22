9cb85cc563f83c4da06529c6ddcc0623aea18809
Angular CLI <angular-cli@angular.io>
2017-04-22 07:15:59 +0200

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularTranslatorPage } from './app.po';
+
+describe('angular-translator App', () => {
+  let page: AngularTranslatorPage;
+
+  beforeEach(() => {
+    page = new AngularTranslatorPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
