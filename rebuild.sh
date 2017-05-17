6daa641a7ef3c350bae14177e1bf8f4cd1dabead
pmartin <contact@elol.fr>
2017-05-17 16:21:09 +0200

    debug i18n + univ

--- rebuild.sh
+++ rebuild.sh
@@ -6,7 +6,7 @@ find . -name "*.ngfactory.ts" -delete
 find . -name "*.ngsummary.json" -delete
 find . -name "*.ngstyle.ts" -delete
 
-./node_modules/.bin/ngc
+./node_modules/.bin/ngc --i18nFile=src/i18n-xlf/messages.$LANG.xlf --i18nFormat=xlf --locale=$LANG
 webpack
 mkdir -p final/$LANG
 cp -R dist/$LANG/* final/$LANG
