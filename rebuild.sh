3f77e6c887be8a9983dfea537886091d4f16bd16
pmartin <contact@elol.fr>
2017-05-17 15:14:52 +0200

    build

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
+LANG=$1
+
 find . -name "*.ngfactory.ts" -delete
 find . -name "*.ngsummary.json" -delete
 find . -name "*.ngstyle.ts" -delete
 
 ./node_modules/.bin/ngc
 webpack
+cp -R dist/$LANG/* final/$LANG
+node dist/main.js dist/$LANG/index.html final/$LANG /
+node dist/main.js dist/$LANG/index.html final/$LANG about
